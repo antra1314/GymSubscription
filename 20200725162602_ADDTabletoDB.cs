8e66af3943bd9f97de4c11cc3bfa23a5b8f045d0
antra1314 <36056952+antra1314@users.noreply.github.com>
2020-07-27 23:47:55 +0100

    Migration Files

---
+++ 20200725162602_ADDTabletoDB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StayFitGym.Migrations
+{
+    public partial class ADDTabletoDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserDetails",
+                columns: table => new
+                {
+                    UserID = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    DOB = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDetails", x => x.UserID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserDetails");
+        }
+    }
+}
